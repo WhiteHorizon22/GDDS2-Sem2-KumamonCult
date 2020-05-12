fc995bc8f3cabedbbb3cb8a997c06ac9e7ac2e9f
Lsyyy16 <64509041+Lsyyy16@users.noreply.github.com>
2020-05-12 15:55:41 +0800

    Added PauseScreen script

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PauseScreen : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    private PlayerController player;
+    private LevelManager theManager;
+    public AudioMixer audioMixer;
+
+    public static bool GamePaused = false;
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseScreen.SetActive(true);
+        theManager.levelMusic.Pause();
+        Debug.Log(Time.timeScale);
+        GamePaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+        pauseScreen.SetActive(false);
+        theManager.levelMusic.Play();
+        GamePaused = false;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        theManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+}
